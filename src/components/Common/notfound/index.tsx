fa41b4d6bcbf69738de53ae86f01b917e5d3207c
tiennguyen <tienhgm@gmail.com>
2022-08-21 14:18:40 +0700

    404 page

--- src/components/Common/notfound/index.tsx
+++ src/components/Common/notfound/index.tsx
@@ -1,13 +1,20 @@
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 import styles from './style.module.scss';
 export function NotFound() {
+  const { t } = useTranslation();
+  const history = useHistory();
+  const backToHome = () => {
+    history.push('/')
+  }
   return (
     <div className={styles['not-found']}>
       <div className={styles.child}>
         <div>
           <span className={`${styles.text} ${styles.text1}`}>404</span>{' '}
-          <span className={`${styles.text} ${styles.text2}`}>NOT FOUND</span>
+          <span className={`${styles.text} ${styles.text2}`}>{t('common.not_found')}</span>
         </div>
-        <div className={styles.backToHome}>Back to home</div>
+        <div className={styles.backToHome} onClick={backToHome}>{t('common.back_home')}</div>
       </div>
     </div>
   );
